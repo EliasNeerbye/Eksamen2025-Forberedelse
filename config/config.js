d5b2481584d590ce7f5f0e84ddc5f439103eb127
Elias Neerbye <quidque@quidque.no>
2025-06-02 11:51:51 +0200

    Testing deployment

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/userapidb';
 // Example: 3000
 const PORT = process.env.PORT || 3000;
 
-// Example: 'nsadiunsaldiusdiu1n2981ndiasdlsiuahddm12i9heds0a8d9'
+// Example: nsadiunsaldiusdiu1n2981ndiasdlsiuahddm12i9heds0a8d9
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
 // Example: http://localhost:3000,https://yourdomain.com
